b6f76f0f6b7124ff345a2da4ef40b37f0182ef9c
work <work@Brendans-iMac-Desk.local>
2020-11-15 21:14:18 -0600

    update

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,24 @@
+-- Create a list of current potential retiring employees by title
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;    
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+    rt.first_name,
+	rt.last_name,
+    rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, to_date DESC;
